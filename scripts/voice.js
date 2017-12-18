571adad7dba7a7faad10a374710d5b1ee3bcbb2b
Vyacheslav Matyukhin <me@berekuk.ru>
2017-12-18 14:39:41 +0300

    only @berekuk can use The Voice

--- scripts/voice.js
+++ scripts/voice.js
@@ -5,7 +5,10 @@ module.exports = robot => {
       const saying = res.match[1];
       const channelName = res.match[2];
       const channel = res.robot.adapter.client.rtm.dataStore.getChannelOrGroupByName(channelName);
-      console.log(res.message);
+      if (res.message.user.id !== 'U0F6P8JAG') {
+        return;
+      }
+
       robot.adapter.client.web.reactions.add(':white_check_mark:', { channel: res.message.room, timestamp: res.message.id })
       res.robot.messageRoom(channel.id, saying);
     }
