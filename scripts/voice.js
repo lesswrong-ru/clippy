5f0f6d342d58bb07145083e45aff0c3fcdd72d02
Vyacheslav Matyukhin <me@berekuk.ru>
2018-04-05 18:42:26 +0300

    voice is re-enabled for everyone

--- scripts/voice.js
+++ scripts/voice.js
@@ -5,11 +5,11 @@ module.exports = robot => {
       const saying = res.match[1];
       const channelName = res.match[2];
       const channel = res.robot.adapter.client.rtm.dataStore.getChannelOrGroupByName(channelName);
-      if (res.message.user.id !== 'U0F6P8JAG') {
-        return;
-      }
+      //if (res.message.user.id !== 'U0F6P8JAG') {
+      //  return;
+      //}
 
-      robot.adapter.client.web.reactions.add(':white_check_mark:', { channel: res.message.room, timestamp: res.message.id })
+      robot.adapter.client.web.reactions.add(':heavy_check_mark:', { channel: res.message.room, timestamp: res.message.id })
       res.robot.messageRoom(channel.id, saying);
     }
   );
